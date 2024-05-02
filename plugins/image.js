c94c6fbb76de727886e2276259f87aad8cb473d6
Zorobot2006 <156957436+Zorobot2006@users.noreply.github.com>
2024-05-02 01:56:58 +0100

    Update image.js

--- plugins/image.js
+++ plugins/image.js
@@ -1,6 +1,6 @@
 import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-const prohibited = ['', '', '', '', '', '', '', '', 'puto', 'culo', 'putita', '', '', 'pene', 'coño', 'asesinato', '', 'mia khalifa', 'desnudo', 'desnuda', 'cuca', 'chocha', 'muertos', '', '', '', 'teta', 'vagina', 'marsha may', 'misha cross', '', '', 'furro', 'furra', 'xxx', 'rule34', '', '', 'necrofilia', 'pinga', 'horny', '', 'nude', 'popo', '', 'femdom', 'futanari', 'erofeet', '', '', '', 'ero', 'ecchi', '', '', 'ahegao', 'pija', 'verga', 'trasero', 'violation', 'violacion', 'bdsm', 'cachonda', '', 'cp', 'mia marin', 'lana rhoades', 'cepesito', '', 'buceta', '']
+const prohibited = []
 if (prohibited.some(word => m.text.toLowerCase().includes(word))) return conn.reply(m.chat, '🚩 *لن أعطي نتائج لطلبك* 😒', m, )
 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
